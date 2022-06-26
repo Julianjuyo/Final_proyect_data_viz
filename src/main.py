9bb082f2daa907647b2b6ce0c0ae28a2acf132bb
Julian Oliveros Forero <je.oliverosf@uniandes.edu.co>
2022-06-26 10:51:49 +0200

    finish all the grpahs

--- src/main.py
+++ src/main.py
@@ -103,13 +103,7 @@ df_president_candidates_party_max_merge = pd.merge(df_president_candidates_party
 
 
 # ---------------------
-# VIZ 4: Show per state in the last 10 years which states are the most votes if republican or DEMOCRAT (in a map).
-#        All the years show the states that have more republican or democrat Ed in history.
-# ---------------------
-
-
-# ---------------------
-# VIZ 5: Show the percentage of votes from each party for the past years.
+# VIZ 4: Show the percentage of votes from each party for the past years.
 #        Graph that on the x-axis have democrat, republicans and other.
 #        Each of those groups by year.
 #        On the y axis is the number of votes.
@@ -132,7 +126,7 @@ fig_president_per_party_past_years_sum = px.bar(df_president_per_party_past_year
 
 
 # ---------------------
-# VIZ 6: Show the name of the last 10 presidents show their names and show the number of votes they had to win.
+# VIZ 5: Show the name of the last 10 presidents show their names and show the number of votes they had to win.
 # ---------------------
 
 df_president_names_and_percentage = president[[
@@ -161,6 +155,118 @@ df_president_names_and_percentage_new_max_merge1 = pd.merge(df_president_names_a
 #                fill_color='lavender',
 #                align='left'))
 # ])
+
+
+# ---------------------
+# Viz 1: 6. The number of votes per party is detailed historic during all the times.
+# ---------------------
+
+
+df_senate_candidates_party = senate[["candidatevotes", "party_detailed"]]
+
+df_senate_candidates_party_sum = df_senate_candidates_party.groupby(
+    ["party_detailed"]).sum().reset_index(drop=False)
+
+
+df = px.data.tips()
+fig_senate_candidates_party_sum_all = px.treemap(df_senate_candidates_party_sum,
+                                                 path=[px.Constant(
+                                                     "all"), 'party_detailed'],
+                                                 values='candidatevotes',
+                                                 title="The number of votes per party is detailed historic during all the times.")
+
+fig_senate_candidates_party_sum_all.update_traces(root_color="lightgrey")
+fig_senate_candidates_party_sum_all.update_layout(
+    margin=dict(t=50, l=25, r=25, b=25))
+# fig_senate_candidates_party_sum_all.show()
+
+
+# ---------------------
+# Viz 7: The number of votes per party is detailed historic during all the times WITHOUT REPUBLICAN NOR DEMOCRAT for the senate
+# ---------------------
+df_senate_candidates_party_repli = df_senate_candidates_party
+
+df_senate_candidates_party_repli.drop(
+    df_senate_candidates_party_repli.loc[df_senate_candidates_party_repli['party_detailed'] == "DEMOCRAT"].index, inplace=True)
+df_senate_candidates_party_repli.drop(
+    df_senate_candidates_party_repli.loc[df_senate_candidates_party_repli['party_detailed'] == "REPUBLICAN"].index, inplace=True)
+
+
+df_senate_candidates_party_repli_group = df_senate_candidates_party_repli.groupby(
+    ["party_detailed"]).sum().reset_index(drop=False)
+
+
+df = px.data.tips()
+fig_senate_number_votes_per_party_historic_witout = px.treemap(df_senate_candidates_party_repli_group,
+                                                               path=[px.Constant(
+                                                                   "all"), 'party_detailed'],
+                                                               values='candidatevotes',
+                                                               title="The number of votes per party is detailed historic during all the times.")
+
+fig_senate_number_votes_per_party_historic_witout.update_traces(
+    root_color="lightgrey")
+fig_senate_number_votes_per_party_historic_witout.update_layout(
+    margin=dict(t=50, l=25, r=25, b=25))
+# fig_senate_number_votes_per_party_historic_witout.show()
+
+
+# ---------------------
+# VIZ 8: Show per state in all years which states are the most votes if republican or Democratic (in a map).
+#        All the years show the states that have more republican or democrat Ed in history.
+# ---------------------
+
+df_senate_candidates_party = senate[[
+    "year", "state_po", "party_detailed", "candidatevotes"]]
+
+
+df_senate_candidates_party_max = df_senate_candidates_party.groupby(
+    ["year", "state_po"], sort=False)["candidatevotes"].max().reset_index(drop=False)
+
+
+df_senate_candidates_party_max_merge = pd.merge(df_senate_candidates_party, df_senate_candidates_party_max,  how='inner', left_on=[
+    'year', 'state_po', 'candidatevotes'], right_on=['year', 'state_po', 'candidatevotes'])
+
+
+# ---------------------
+# VIZ 9: Show the percentage of votes from each party for the past years.
+#        Graph that on the x-axis have democrat, republicans and other.
+#        Each of those groups by year.
+#        On the y axis is the number of votes.
+# ---------------------
+df_senate_per_party_past_years = senate[[
+    "year", "party_simplified", "candidatevotes"]]
+
+df_senate_per_party_past_years_sum = df_senate_per_party_past_years.groupby(
+    ["year", "party_simplified"]).sum().reset_index(drop=False)
+
+
+wide_df = px.data.medals_wide()
+fig_senate_per_party_past_years_sum = px.bar(df_senate_per_party_past_years_sum,
+                                             x="year",
+                                             y="candidatevotes",
+                                             color="party_simplified",
+                                             title="Total votes per most representative parties for the period 1970 - 2020")
+
+# fig_president_per_party_past_years_sum.show()
+
+
+# ---------------------
+# VIZ 10:  Show the name of the last 10 presidents show their names and show the number of votes they had to win.
+# ---------------------
+
+df_senate_names_and_percentage = senate[[
+    "year", "candidate", "party_detailed", "candidatevotes"]]
+
+
+df_senate_names_and_percentage_new = df_senate_names_and_percentage.groupby(
+    ["year", "party_detailed", "candidate"]).sum().reset_index(drop=False)
+
+df_senate_names_and_percentage_new_max = df_senate_names_and_percentage_new.groupby(
+    ["year"], sort=False)["candidatevotes"].max().reset_index(drop=False)
+
+
+df_senate_names_and_percentage_new_max_merge1 = pd.merge(df_senate_names_and_percentage_new_max, df_senate_names_and_percentage_new,  how='inner', left_on=[
+    'year', 'candidatevotes'], right_on=['year', 'candidatevotes'])
 
 
 # -----------------------------------
@@ -204,9 +310,59 @@ app.layout = html.Div(children=[
     dash_table.DataTable(
         df_president_names_and_percentage_new_max_merge1.to_dict(
             'records'), [{"name": i, "id": i}
-                         for i in df_president_names_and_percentage_new_max_merge1.columns])
+                         for i in df_president_names_and_percentage_new_max_merge1.columns
+                         ]),
+    dcc.Graph(
+        id='fig_senate_candidates_party_sum_all',
+        figure=fig_senate_candidates_party_sum_all
+    ),
+    dcc.Graph(
+        id='fig_senate_number_votes_per_party_historic_witout',
+        figure=fig_senate_number_votes_per_party_historic_witout
+    ),
+    html.Div([
+        html.H4('Polotical candidate voting pool analysis'),
+        html.P("Select a year:"),
+        dcc.RadioItems(
+            id='year_change_2',
+            options=df_senate_candidates_party_max_merge["year"].unique(),
+            value="1976",
+            inline=True
+        ),
+        dcc.Graph(id="graph_map_senate"),
+    ]),
+    dcc.Graph(
+        id='fig_senate_per_party_past_years_sum',
+        figure=fig_senate_per_party_past_years_sum
+    ),
+    dash_table.DataTable(
+        df_senate_names_and_percentage_new_max_merge1.to_dict(
+            'records'), [{"name": i, "id": i}
+                         for i in df_senate_names_and_percentage_new_max_merge1.columns
+                         ]),
+
 
 ])
+
+
+@app.callback(
+    Output("graph_map_senate", "figure"),
+    Input("year_change_2", "value"))
+def display_choropleth(input_value):
+    df_senate_candidates_party_max_merge_2 = df_senate_candidates_party_max_merge.copy()
+
+    print(df_senate_candidates_party_max_merge["year"].unique())
+    print(input_value)
+    df_senate_candidates_party_max_merge_2.drop(
+        df_senate_candidates_party_max_merge_2.loc[df_senate_candidates_party_max_merge_2['year'] != int(input_value)].index, inplace=True)
+
+    print(df_senate_candidates_party_max_merge_2)
+
+    fig = px.choropleth(locations=df_senate_candidates_party_max_merge_2["state_po"],
+                        locationmode="USA-states",
+                        color=df_senate_candidates_party_max_merge_2["party_detailed"],
+                        scope="usa")
+    return fig
 
 
 @app.callback(
